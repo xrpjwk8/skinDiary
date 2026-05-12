5f32e0334dee02e411db03faa0a0dc16596a7268
xrpjwk8 <83jason@naver.com>
2026-05-12 21:53:14 +0900

    Initial Commit

---
+++ SkinDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkinDiary
+//
+//  Created by Jaewon Kim on 5/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkinDiary/SkinDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinDiaryApp.swift
+//  SkinDiary
+//
+//  Created by Jaewon Kim on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct SkinDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
